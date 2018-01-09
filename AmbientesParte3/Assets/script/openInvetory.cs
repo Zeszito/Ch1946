3060c5b449a90e2b8606d4509520339c10f06478
Moisés <moises.moreira.bio@gmail.com>
2018-01-09 13:21:25 +0000

    canvas e assim

---
+++ AmbientesParte3/Assets/script/openInvetory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openInvetory : MonoBehaviour {
+    public GameObject canvas;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (canvas.GetComponent<Canvas>().enabled == false)
+        {
+            //Debug.Log("entrei");
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                canvas.GetComponent<Canvas>().enabled = true;
+               
+            }
+        }
+
+        else if (canvas.GetComponent<Canvas>().enabled == true)
+        {
+
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                Cursor.visible = false;
+                canvas.GetComponent<Canvas>().enabled = false;
+
+            }
+        }
+
+    }
+
+}
